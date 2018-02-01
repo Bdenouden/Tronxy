80325a26cb2634e262b37b211942f6079038cdd5
fysfys <33008159+fysfys@users.noreply.github.com>
2018-02-01 15:48:55 +0100

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -76,7 +76,7 @@
 // build by the user have been successfully uploaded into firmware.
 #define STRING_CONFIG_H_AUTHOR "(Bram den Ouden)" // Who made the changes.
 #define SHOW_BOOTSCREEN
-#define STRING_SPLASH_LINE1 "(TronXY X1)" 				// will be shown during bootup in line 1
+#define STRING_SPLASH_LINE1 "TronXY X1" 				// will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 SHORT_BUILD_VERSION         // will be shown during bootup in line 2
 
 //
